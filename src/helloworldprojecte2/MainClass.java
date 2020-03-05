0b73f049958d6120edf73bd6563b9beadb2c7617
Munshi Sadman Sakib Annoor <61384572+annoorsakib@users.noreply.github.com>
2020-03-06 00:03:11 +0600

    Add files via upload

---
+++ src/helloworldprojecte2/MainClass.java
@@ -0,0 +1,12 @@
+
+package helloworldprojecte2;
+public class MainClass {
+
+    public static void main(String[] args){
+        // TODO code application logic here
+        for( int i=1;i<=50000;i++){
+        System.out.println(i+"hello world!");
+        }
+    }
+    
+}
